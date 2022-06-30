e60ece9823e1c9664e62d48b9c0d988cc35ab6f2
robynroby <robinsonmwaura254@gmail.com>
2022-06-30 06:45:34 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
 import Home from './Components/Home/Home';
-// import {
-// 	BrowserRouter as Router,
-// 	Routes,
-// 	Route,
-// 	Link
-// } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Tenants from './Components/Tenants/Tenants';
 
